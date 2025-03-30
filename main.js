e64681b8af28b7d2d0dad3b43a6c12d9c62c6a9b
Svetozar Stoev <zarkoto23@gmail.com>
2025-03-30 18:37:45 +0300

    game structure

---
+++ main.js
@@ -0,0 +1,54 @@
+//initialization game state
+const state = {
+  player: "Pesho",
+  wizard: {
+    x: 100,
+    y: 100,
+    width: 50,
+    height: 50,
+  },
+};
+
+//obj creation
+const gameArea = document.querySelector(".game-area");
+const factory = {
+  createWizard(wizard) {
+    //create El
+    const wizardElement = document.createElement("div");
+
+    //set styles
+    wizardElement.style.width = wizard.width + "px";
+    wizardElement.style.height = wizard.height + "px";
+    wizardElement.style.backgroundImage = 'url("images/wizard.png")';
+    wizardElement.style.backgroundSize = "contain";
+    wizardElement.style.backgroundRepeat = "no-repeat";
+    wizardElement.style.backgroundPosition = "center";
+
+    gameArea.appendChild(wizardElement)
+
+    //set position
+
+    wizardElement.style.position='absolute'
+    wizardElement.style.left=wizard.x+'px'
+    wizardElement.style.top=wizard.y+'px'
+
+    //attach to dom
+  },
+};
+//input control
+
+//game loop
+
+//game frames
+function newFrame() {
+
+}
+
+const startElement = document.querySelector(".game-start");
+startElement.addEventListener("click", (e) => {
+  e.currentTarget.classList.add("hidden");
+
+  factory.createWizard(state.wizard);
+});
+
+// init game
