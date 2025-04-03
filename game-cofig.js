af01313f5f19d17f624c78dc12ba7cbbd7129100
Svetozar Stoev <zarkoto23@gmail.com>
2025-04-03 11:52:37 +0300

    modifywizardPosition func

---
+++ game-cofig.js
@@ -0,0 +1,4 @@
+export const config={
+  speed:3,
+
+}
--- game-engine.js
+++ game-engine.js
@@ -1,29 +1,33 @@
 import { state } from "./game-state.js";
+import { config } from "./game-cofig.js";
 export { newFrame }; //
 
 //game frames
 function newFrame() {
-  const wizardElement = document.querySelector(".wizard");
-  //wizard moves
-
-  if (state.controls.KeyA) {
-    wizardElement.style.left = `${state.wizard.x--}px`;
-  }
-  if (state.controls.KeyD) {
-    wizardElement.style.left = `${state.wizard.x++}px`;
-  }
-  if (state.controls.KeyW) {
-    wizardElement.style.top = `${state.wizard.y--}px`;
-  }
-  if (state.controls.KeyS) {
-    wizardElement.style.top = `${state.wizard.y++}px`;
-  }
+  modifyWizardPosition()
 
   if (!state.isGameOver) {
     window.requestAnimationFrame(newFrame);
   }
 }
+function modifyWizardPosition(){
+  const wizardElement = document.querySelector(".wizard");
+  //wizard moves
 
+  if (state.controls.KeyA) {
+    wizardElement.style.left = `${state.wizard.x-=config.speed}px`;
+  }
+  if (state.controls.KeyD) {
+    wizardElement.style.left = `${state.wizard.x+=config.speed}px`;
+  }
+  if (state.controls.KeyW) {
+    wizardElement.style.top = `${state.wizard.y-=config.speed}px`;
+  }
+  if (state.controls.KeyS) {
+    wizardElement.style.top = `${state.wizard.y+=config.speed}px`;
+  }
+
+}
 export const engine = {
   start() {
     window.requestAnimationFrame(newFrame);
